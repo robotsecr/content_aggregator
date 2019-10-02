5c92f4d16613652664bbb212729878b43fddb523
robotsecr <robot20192020@gmail.com>
2019-10-03 01:51:09 +0200

    first commit

---
+++ content_aggregator.py
@@ -0,0 +1,26 @@
+import requests
+from urllib.request import urlopen as uReq
+import bs4
+from bs4 import BeautifulSoup as soup
+page_url='https://www.walmart.com/search/?cat_id=1085666_1007040'
+client=uReq(page_url)
+html_page=client.read()
+client.close()
+page_soup=soup(html_page,"html.parser")#html parser
+containers_title=page_soup.findAll("div",{"class","search-result-product-title gridview"})
+containers_price_characteristic=page_soup.findAll("span",{"class","price-characteristic"})
+containers_mantissa=page_soup.findAll("span",{"class","price-mantissa"})
+filename="products.csv"
+headers="prodect,price\n"
+f=open(filename,"w")
+f.write(headers)
+print(len(containers_title))
+for i in range(len(containers_title)):
+	title=containers_title[i].a["title"]
+	container_charac=containers_price_characteristic[i].text
+	container_mantissa=containers_mantissa[i].text
+	print("product_name:"+str(title))
+	print("price:"+"$"+str(container_charac)+"."+str(container_mantissa))
+	f.write(title.replace(",","_")+","+"$"+container_charac+"."+container_mantissa+"\n")
+f.close()
+                                         #By Miss.Robot
